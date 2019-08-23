bd0b13d78cf85cf85ce2065784d4c4681657294f
Jennifer Muniz <Jen@Jennifers-MacBook-Pro-3.local>
2019-08-23 17:09:00 -0400

    First commit - Jennifer Muniz

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,107 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
 
+
+
+//Get full character list
+ getFullList () {
+
+  //Get list from local host
+   axios.get('http://localhost:8000/characters')
+    .then ( response => {
+      console.log(response.data);
+      console.log(response.data.length);
+      
+      //Loop through character list
+      let characters = ``;
+      for(let i=0; i<response.data.length; i++){
+        
+        //Put each character info in HTML format
+        characters += `<div class="character-info">
+        <div class="name">${response.data[i].name}</div>
+        <div class="occupation">${response.data[i].occupation} Occupation</div>
+        <div class="cartoon">${response.data[i].cartoon}</div>
+        <div class="weapon">${response.data[i].weapon}</div>
+        </div>`;
+        
+        //Add characters to HTML
+        document.querySelector("body > section:nth-child(1) > div").innerHTML = characters;
+      }
+
+    })
   }
 
-  getOneRegister () {
 
+
+
+
+
+
+
+  //Get one character by ID
+  getOneRegister (id) {
+  
+    //Get character info from local host
+    axios.get(`http://localhost:8000/characters/${id}`)
+      .then ( response => {
+
+        //Put character info in HTML format
+        let character = `<div class="character-info">
+        <div class="name">${response.data.name}</div>
+        <div class="occupation">${response.data.occupation}</div>
+        <div class="cartoon">${response.data.cartoon}</div>
+        <div class="weapon">${response.data.weapon}</div>
+        </div>`; 
+
+        //Add characters to HTML
+        document.querySelector("body > section:nth-child(1) > div").innerHTML = character;
+      })
   }
 
-  createOneRegister () {
 
+
+
+
+
+
+
+  //Create new character
+  createOneRegister (newCharacter) {
+
+    //Add new character to list
+    axios.post('http://localhost:8000/characters', newCharacter)
+      .then( response => {})
   }
 
-  updateOneRegister () {
 
+
+
+
+
+
+
+
+  //Edit character
+  updateOneRegister (updatedCharacter, id) {
+
+    axios.put(`http://localhost:8000/characters/${id}`, updatedCharacter)
+    .then( response => {})
   }
 
-  deleteOneRegister () {
 
+
+
+
+
+
+
+
+
+  //Delete character
+  deleteOneRegister (id) {
+
+    axios.delete(`http://localhost:8000/characters/${id}`)
+      .then( response => {})
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,76 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
+
+$(document).ready( () => {
+
+
+
+  //Get full character list
+  document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList();
   }
   
+
+
+
+  //Get character using ID
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.querySelector("body > section:nth-child(1) > section > div:nth-child(2) > input[type=text]").value;
+    charactersAPI.getOneRegister(id);
   }
   
+
+
+
+
+  //Delete character using ID
   document.getElementById('delete-one').onclick = function(){
-        
+    
+    let id = document.querySelector("body > section:nth-child(1) > section > div.operation.delete > input[type=text]").value;
+    charactersAPI.deleteOneRegister(id);
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+
+
+
+  //Edit character using ID
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault();    
+    let updatedCharacter = {
+      id: document.querySelector("#edit-character-form > div:nth-child(1) > input[type=text]").value,
+      name: document.querySelector("#edit-character-form > div:nth-child(2) > input[type=text]").value,
+      occupation: document.querySelector("#edit-character-form > div:nth-child(3) > input[type=text]").value,
+      cartoon: document.querySelector("#edit-character-form > div:nth-child(5) > input[type=checkbox]").checked,
+      weapon: document.querySelector("#edit-character-form > div:nth-child(4) > input[type=text]").value
+    }
+
+
+
+    console.log(updatedCharacter.cartoon);
+    charactersAPI.updateOneRegister(updatedCharacter, updatedCharacter.id);
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+
+
+
+
+  //Create new character
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault()
+    let newCharacter = {
+      name: document.querySelector("#new-character-form > div:nth-child(1) > input[type=text]").value,
+      occupation: document.querySelector("#new-character-form > div:nth-child(2) > input[type=text]").value,
+      cartoon: document.querySelector("#new-character-form > div:nth-child(4) > input[type=checkbox]").checked,
+      weapon: document.querySelector("#new-character-form > div:nth-child(3) > input[type=text]").value
+    }
+
+    console.log(newCharacter);
+
+    charactersAPI.createOneRegister(newCharacter);      
   }
 })
+
+
